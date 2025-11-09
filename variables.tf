0f7030ca2086f40c544c45b9d4ddbfb9ccfd8137
k-candidate <k-candidate@outlook.com>
2025-11-09 23:24:21 +0100

    --allow-empty

---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "project_id" {
+  description = "The ID of the project in which to provision resources."
+  type        = string
+  default     = "qwiklabs-gcp-01-0b750d7737a2"
+}
+
+variable "name" {
+  description = "Name of the buckets to create."
+  type        = string
+  default     = "qwiklabs-gcp-01-0b750d7737a2"
+}
